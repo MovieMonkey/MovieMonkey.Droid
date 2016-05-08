14805c341eda02b2d3ed32e65d4348c93d572e9f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-05-08 12:02:29 +0200

    fix menu

---
+++ app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/Fragments/MovieListFragment.java
@@ -0,0 +1,22 @@
+package gruppenprojekt.mobpro.hslu.moviemanager.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class MovieListFragment extends Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MovieListFragment.
+     */
+    public static MovieListFragment newInstance() {
+        return new MovieListFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/MainActivity.java
+++ app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/MainActivity.java
@@ -1,13 +1,81 @@
 package gruppenprojekt.mobpro.hslu.moviemanager;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import gruppenprojekt.mobpro.hslu.moviemanager.Fragments.MovieListFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                    drawerLayout,
+                    toolbar,
+                    R.string.text_drawer_open,
+                    R.string.text_drawer_close);
+
+            drawerLayout.addDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.setCustomAnimations(android.support.design.R.anim.abc_fade_in, android.support.design.R.anim.abc_fade_out);
+        String tag = "";
+
+        if (id == R.id.nav_all) {
+            MovieListFragment fragment = MovieListFragment.newInstance();
+            tag = fragment.getTag();
+
+            transaction.replace(R.id.content_frame, fragment, tag);
+
+        } else if (id == R.id.nav_favorites) {
+
+        } else if (id == R.id.nav_backup) {
+
+        }
+        transaction.addToBackStack(tag);
+        transaction.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
