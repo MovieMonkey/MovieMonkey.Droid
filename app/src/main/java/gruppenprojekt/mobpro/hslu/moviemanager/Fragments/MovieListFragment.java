0b4e2e5d8271b1d494f3b03c1f53eb4729086cdd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-05-15 13:19:29 +0200

    add navigation via floating point

--- app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/Fragments/MovieListFragment.java
+++ app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/Fragments/MovieListFragment.java
@@ -1,12 +1,15 @@
 package gruppenprojekt.mobpro.hslu.moviemanager.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import gruppenprojekt.mobpro.hslu.moviemanager.R;
+import gruppenprojekt.mobpro.hslu.moviemanager.SearchActivity;
 
 public class MovieListFragment extends Fragment {
 
@@ -20,12 +23,24 @@ public class MovieListFragment extends Fragment {
         return new MovieListFragment();
     }
 
+    private FloatingActionButton floatingActionButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_list, container, false);
+
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab_add);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SearchActivity.class));
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/MainActivity.java
+++ app/src/main/java/gruppenprojekt/mobpro/hslu/moviemanager/MainActivity.java
@@ -1,5 +1,7 @@
 package gruppenprojekt.mobpro.hslu.moviemanager;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import gruppenprojekt.mobpro.hslu.moviemanager.Fragments.MovieListFragment;
 
